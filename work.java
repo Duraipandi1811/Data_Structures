2ff791aea677410ca1385784b6f047fe448ef297
Durai pandi <130584388+Duraipandi1811@users.noreply.github.com>
2025-04-26 22:36:53 +0530

    Add files via upload

---
+++ work.java
@@ -0,0 +1,16 @@
+public class work
+{
+	public static void main(String[] args)
+	{
+	    int a=13;
+	    int b=7;
+	    int and_fun=a%b;
+	    int or_fun=a|b;
+	    int xor_fun=a^b;
+	    int d=13,e=4;
+		System.out.println("and_Value:"+and_fun);
+		System.out.println("Or_Value:"+or_fun);
+		System.out.println("Xor_Value:"+xor_fun);
+		System.out.println("Right shift:"+);
+	}
+}
